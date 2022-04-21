438c294dade45997f4b06c48ba8c830e5493172b
Yanochka <zavgorodnyaya-yana@mail.ru>
2022-04-21 14:24:20 +0300

    Скорректировала код в папке js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const сalorieCounter = () => {
+    return document.querySelectorAll(".radio");
+}
+
+console.log(сalorieCounter());
+
+const isDisabled = () => {
+	return button.hasAttribute("disabled");
+}
+
+
+console.log(isDisabled()); // 
+
